39d5fce53ad4d747c5d83c4e913e01131bee5739
legalvao1 <legalvao1@gmail.com>
2021-06-08 18:04:38 -0300

    Requisito 1 - Pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <main>
+        <h1>Login</h1>
+        <input data-testid="email-input" placeholder="Digite seu e-mail" />
+        <input data-testid="password-input" placeholder="Digite sua senha" />
+        <button type="button" disabled>Entrar</button>
+      </main>
+    );
   }
 }
 
