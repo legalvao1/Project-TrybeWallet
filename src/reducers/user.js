64dcbd894949f3e37c856ccf79a86d1b942ff20d
legalvao1 <legalvao1@gmail.com>
2021-06-08 16:06:39 -0300

    Configuraçao inicial userReducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,20 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'LOGIN':
+    return {
+      ...state,
+      users: { email: action.payload },
+    };
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
