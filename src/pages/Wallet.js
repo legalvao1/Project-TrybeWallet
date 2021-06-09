189a8af5b06b855534dd3d265268a42a4f8cc37c
legalvao1 <legalvao1@gmail.com>
2021-06-09 08:53:53 -0300

    Configuraçao inicial do header da wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <main>
+        <header>
+          <div>
+            <h3>TrybeWallet</h3>
+          </div>
+          <div>
+            <span data-testid="email-field">email do reducer</span>
+            <span data-testid="total-field">Despesa total: R$ 0,00</span>
+            <span data-testid="header-currency-field">BRL</span>
+          </div>
+        </header>
+
+      </main>
+    );
   }
 }
 
